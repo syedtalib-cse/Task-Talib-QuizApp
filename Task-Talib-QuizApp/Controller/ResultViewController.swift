a54c22a89ff8bf15346770fe3aee33dde994449f
Syed Abu Talib <abdulmazeedshaik45@gmail.com>
2021-04-07 19:04:02 +0530

    gffh

--- Task-Talib-QuizApp/Controller/ResultViewController.swift
+++ Task-Talib-QuizApp/Controller/ResultViewController.swift
@@ -74,7 +74,7 @@ extension ResultViewController:UITableViewDelegate,UITableViewDataSource {
         
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 130
     }
     
     
